f27e6629a45a32cdce738ea2bdc472deb13372ae
vy3191 <venkyafhs@gmail.com>
2020-08-30 19:31:20 -0400

    Initial commit

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,18 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.storage = [None] * capacity
+        self.capacity = capacity
+        self.length = 0
 
     def append(self, item):
-        pass
+        if len(self.storage) <= self.capacity:
+            self.storage[self.length] = item
+            self.length += 1
+        else:
+            self.length = 0
+            self.storage[self.length] = item
+            self.length += 1    
 
     def get(self):
-        pass
+        for item in self.storage:
+            print(item)

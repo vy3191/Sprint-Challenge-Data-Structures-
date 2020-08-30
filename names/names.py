11ecc87ce4bb62dc37053116323e1fd3e542d10f
vy3191 <venkyafhs@gmail.com>
2020-08-30 19:54:19 -0400

    Completed Runtime Optimization assignment

--- names/names.py
+++ names/names.py
@@ -13,10 +13,31 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
+
+#Try 1 --->  1.13496 seconds in my laptop
+# duplicates = [name for name in names_1 if name in names_2]
+
+
+#Try 2 ---> 0.0059828 seconds in my machine
+
+#1)Loop trough the name_1 list  
+# 2)store the names in the dictionary as keys 
+# 3) loop through the second list 
+# 4) if name in a second list found in the dictionary then push it to the dictionary.
+duplicate_dict = {}
+for name in names_1:
+    duplicate_dict[name] = True
+for name in names_2:
+    if name in duplicate_dict:
+        duplicates.append(name)
+
+
+
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")

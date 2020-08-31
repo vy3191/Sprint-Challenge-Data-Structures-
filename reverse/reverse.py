8393e22baeec46c619ad69103a97e32e31f4020d
vy3191 <venkyafhs@gmail.com>
2020-08-30 20:34:15 -0400

    Modified add_to_head method and added print method

--- reverse/reverse.py
+++ reverse/reverse.py
@@ -15,14 +15,18 @@ class Node:
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.count = 0
 
     def add_to_head(self, value):
         node = Node(value)
 
         if self.head is not None:
             node.set_next(self.head)
-
-        self.head = node
+            self.head = node
+        else:
+            node.set_next(self.head)
+            self.head = node
+        self.count += 1        
 
     def contains(self, value):
         if not self.head:
@@ -45,25 +49,13 @@ class LinkedList:
         current = self.head
         nodes = []
         while current:
-            if current == self.head:
+            if current is self.head:
                 nodes.append(f"[Head:{current.value}]")
-                current = current.next_node
-            elif current == self.tail:
-                nodes.append(f"[Tail:{current.value}]")
-                current = current.next_node
+                current = current.next_node            
             else:
                 nodes.append(f"[{current.value}]")
                 current = current.next_node 
-            return '-->'.join(nodes)
+        return '-->'.join(nodes)
 
-bst = LinkedList(1)
-
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(14)
-bst.insert(20)
 
 
